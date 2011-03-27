f487eae328b4ab9c13c6f3c48d676687a2956f97
Carl Brown <carlb@pobox.com>
2011-03-27 18:27:32 -0500

    stick branch in dropbox if we find empty dir

--- sync_from_Dropbox.sh
+++ sync_from_Dropbox.sh
@@ -15,19 +15,91 @@ do
 	src_dir="`echo $line | sed -e 's/=.*$//'`"
 	origin_url="`echo $line | sed -e 's/^[^=]*=//'`"
 
+	#If we don't have a copy of this project in our SCM Working Dir, check it out
 	if [ ! -d "${SCM_WORKING_DIR}/$src_dir" ] ; then
 		echo "Checking out into working dir"
 		cd "${SCM_WORKING_DIR}"
 		git clone $origin_url $src_dir
 	fi
 	
+	#Get the latest updates from source control
 	cd "${SCM_WORKING_DIR}/$src_dir"
 	git fetch
 
+	#If this project doesn't have a corresponding folder in Dropbox, make one
 	if [ ! -d "${XCAB_HOME}/$src_dir" ] ; then
 		mkdir "${XCAB_HOME}/$src_dir"
 	fi
-	git branch -a > "${XCAB_HOME}/$src_dir/branches.txt"
+	
+	#Update the list of available branches so the user can find them by looking at Dropbox
+	git branch -a | sed -e 's/^..//' -e 's/ ->.*$//' -e 's,^remotes/,,' > "${XCAB_HOME}/$src_dir/branches.txt"
+	
+	cd "${XCAB_HOME}/$src_dir"
+	
+	for entry in * ; do
+		active_branch=""
+		if [ -d "$entry" ] ; then
+			#This is a directory - we need to decide if we need to do anything with this
+			cd "${XCAB_HOME}/$src_dir/$entry"
+			item_count="`ls -1 | wc -l | sed -e 's/[^0-9]//g'`"
+			cd "${XCAB_HOME}/$src_dir"
+			
+			if [ "$item_count" == "0" ] ; then
+				#Empty directory, need to check the correct branch out into it
+				
+				rm -rf tmp_checkout_dir
+				mv $entry tmp_checkout_dir
+				cd tmp_checkout_dir
+				GIT_DIR="${SCM_WORKING_DIR}/$src_dir/.git"
+				export GIT_DIR
+				
+				#Now we need to figure out the right branch
+				if [ -f "${GIT_DIR}/refs/heads/$entry" ] ; then
+					#Branch exists, this is the one we want
+					active_branch="$entry"
+				elif [ -f "${GIT_DIR}/refs/remotes/origin/$entry" ] ; then
+						#Branch exists, but is remote
+						active_branch="$entry"
+						echo "Creating Local branch '$entry' from remote branch 'origin/$entry'"
+						git branch "$entry" "origin/$entry"
+				else 
+					#directory doesn't match an existing branch
+					for potential_branch in `grep -v '/' "${XCAB_HOME}/$src_dir/branches.txt"`; do
+						substring_match=`echo $entry | grep "^potential_branch"`
+						if [ x"substring_match" != "x" ] ; then
+							active_branch=potential_branch
+						fi
+					done
+					if [ x"$active_branch" != "x" ] ; then
+						git branch "$entry" "$active_branch"
+						active_branch="$entry"
+					else
+						#Do the same thing with remote branches
+						for potential_branch in `grep '/' "${XCAB_HOME}/$src_dir/branches.txt"`; do
+							localized_potential_branch="`echo $potential_branch | sed -e 's,^[^/]*/,,'`"
+							substring_match=`echo $entry | grep "^localized_potential_branch"`
+							if [ x"substring_match" != "x" ] ; then
+								active_branch=potential_branch
+							fi
+						done
+						if [ x"$active_branch" != "x" ] ; then
+							git branch "$entry" "$active_branch"
+							active_branch="$entry"
+						fi
+					fi
+				fi
+				if [ x"$active_branch" == "x" ] ; then
+					echo "Could not figure out an active branch - using master"
+					git branch "$entry" master
+					active_branch="$entry"
+				fi
+				git reset --hard $active_branch
+				cd ..
+				#TODO - wait for Dropbox to finish syncing
+				mv tmp_checkout_dir "$entry"
+			fi
+		fi
+	done 
 
 done
 
