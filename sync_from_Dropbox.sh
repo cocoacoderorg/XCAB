fc89cad092713da95f02da917b37d6233a98497d
Carl Brown <carlb@pobox.com>
2011-03-27 16:48:51 -0500

    first script - initial functionality

---
+++ sync_from_Dropbox.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+DROPBOX_HOME="$HOME/Dropbox"
+XCAB_HOME="${DROPBOX_HOME}/`cat ${DROPBOX_HOME}/.com.PDAgent.XCAB.settings`"
+XCAB_CONF="${XCAB_HOME}/XCAB.conf"
+SCM_WORKING_DIR="$HOME/src"
+
+if [ ! -f "${XCAB_HOME}" ] ; then
+	mkdir "${XCAB_HOME}"
+fi
+
+exec<$XCAB_CONF
+while read line 
+do
+	src_dir="`echo $line | sed -e 's/=.*$//'`"
+	origin_url="`echo $line | sed -e 's/^[^=]*=//'`"
+
+	if [ ! -f "${SCM_WORKING_DIR}/$src_dir" ] ; then
+		echo "Checking out into working dir"
+		cd "${SCM_WORKING_DIR}"
+		git clone $origin_url $src_dir
+	fi
+	
+	cd "${SCM_WORKING_DIR}/$src_dir"
+	git fetch
+
+	if [ ! -f "${XCAB_HOME}/$src_dir" ] ; then
+		mkdir "${XCAB_HOME}/$src_dir"
+	fi
+	git branch -a > "${XCAB_HOME}/$src_dir/branches.txt"
+
+done
+
