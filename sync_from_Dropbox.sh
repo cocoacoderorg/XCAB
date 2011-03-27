d228a78a91f810079fc203131bf06e58769bfeb4
Carl Brown <carlb@pobox.com>
2011-03-27 16:50:42 -0500

    checking for directories not files

--- sync_from_Dropbox.sh
+++ sync_from_Dropbox.sh
@@ -5,7 +5,7 @@ XCAB_HOME="${DROPBOX_HOME}/`cat ${DROPBOX_HOME}/.com.PDAgent.XCAB.settings`"
 XCAB_CONF="${XCAB_HOME}/XCAB.conf"
 SCM_WORKING_DIR="$HOME/src"
 
-if [ ! -f "${XCAB_HOME}" ] ; then
+if [ ! -d "${XCAB_HOME}" ] ; then
 	mkdir "${XCAB_HOME}"
 fi
 
@@ -15,7 +15,7 @@ do
 	src_dir="`echo $line | sed -e 's/=.*$//'`"
 	origin_url="`echo $line | sed -e 's/^[^=]*=//'`"
 
-	if [ ! -f "${SCM_WORKING_DIR}/$src_dir" ] ; then
+	if [ ! -d "${SCM_WORKING_DIR}/$src_dir" ] ; then
 		echo "Checking out into working dir"
 		cd "${SCM_WORKING_DIR}"
 		git clone $origin_url $src_dir
@@ -24,7 +24,7 @@ do
 	cd "${SCM_WORKING_DIR}/$src_dir"
 	git fetch
 
-	if [ ! -f "${XCAB_HOME}/$src_dir" ] ; then
+	if [ ! -d "${XCAB_HOME}/$src_dir" ] ; then
 		mkdir "${XCAB_HOME}/$src_dir"
 	fi
 	git branch -a > "${XCAB_HOME}/$src_dir/branches.txt"
