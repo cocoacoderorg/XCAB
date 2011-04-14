ae1b6a21da30155d7048a46b36d29dfedbc2657d
Carl Brown <carlb@pobox.com>
2011-04-13 19:08:53 -0500

    use a lock file instead of process counting

--- run_from_cron.sh
+++ run_from_cron.sh
@@ -1,46 +1,96 @@
 #!/bin/sh
 
-#give ourselves a little delay 
-sleep 2
+PREFS_DIR="$HOME/Library/Application Support/XCAB"
+ERROR_EMAIL="carlb@ftlv.com"
+SUCCESS_EMAIL="pdagent@me.com"
 
-#TODO use a lock file here instead
-#Check for the number of different start times - that way if we're fork()ing, we don't show up as ourselves
-count_of_me_running="`ps auxwwww | grep run_from_cron.sh | grep -v grep | awk '{print $9}' | sort -u | wc -l`"
+my_dir="`dirname \"$0\"`"
+cd "$my_dir"; pwd
+my_name="`basename \"$0\"`"
 
-if [ "$count_of_me_running" -ne "1" ] ; then
-	echo "Bad count of running copies of this script $count_of_me_running - Bailing!"  >&2
-	output_of_me_running="`ps auxwwww | grep run_from_cron.sh | grep -v grep`"
-	echo "Bad count of running copies of this script $count_of_me_running - Bailing! (ps output: '$output_of_me_running')" | mail -s "run_XCAB_cron error" carlb@ftlv.com
+
+
+if [ ! -d "$PREFS_DIR" ] ; then
+	mkdir -p "$PREFS_DIR"
+fi
+if [ ! -d "$PREFS_DIR/logs" ] ; then
+	mkdir -p "$PREFS_DIR/logs"
+fi
+if [ ! -d "$PREFS_DIR/run" ] ; then
+	mkdir -p "$PREFS_DIR/run"
+fi
+
+TEMPFILE="$PREFS_DIR/run/$my_name.$$"
+LOCKFILE="$PREFS_DIR/run/$my_name.lock"
+START_LOG="$PREFS_DIR/logs/${my_name}_start.log"
+RUN_LOG="$PREFS_DIR/logs/${my_name}.log"
+
+#Make a temp file with our pid in it
+echo $$ > "$TEMPFILE" 2>/dev/null
+if [ $? -ne 0 ] ; then
+	echo "Could not create $TEMPFILE" >&2
+	echo "Could not create $TEMPFILE" | mail -s "$0 error" "$ERROR_EMAIL"
 	exit 5
 fi
 
-cd /Users/carlb/src/XCAB
-bin=`dirname "$0"`
-bin=`cd "$bin"; pwd`
-echo "$0 run starting at `date`" > /tmp/run_XCAB_cron_start.log 2>&1
+#Now atomically create our lockfile
+ln "$TEMPFILE" "$LOCKFILE"  > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+	#lockfile created, remove the temp file, then continue on
+	rm -f "$TEMPFILE"
+else
+	#Couldn't create the lock file, see if the process that made it is still around 
+	#	by sending it a dummy signal
+	kill -0 `cat "$LOCKFILE"` > /dev/null 2>&1
+	if [ $? -eq 0 ] ; then
+		#process received the signal, so still running, so we give up
+		rm -f "$TEMPFILE"
+		exit 0
+	else
+		echo "Removing stale lock file"
+		rm -f "$LOCKFILE"
+		#Now atomically create our lockfile again
+		ln "$TEMPFILE" "$LOCKFILE"  > /dev/null 2>&1
+		if [ $? -eq 0 ] ; then
+			#lockfile created, remove the temp file, then continue on
+			rm -f "$TEMPFILE"
+		else
+			echo "Could not create lock file, even after removing stale one" >&2
+			echo "Could not create lock file, even after removing stale one" | mail -s "$0 error" "$ERROR_EMAIL"
+			rm -f "$TEMPFILE"
+			exit 5
+		fi
+	fi
+fi
 
-rm /tmp/run_XCAB_cron.log && touch /tmp/run_XCAB_cron.log
+echo "$0 run starting at `date`" > "$START_LOG" 2>&1
+
+rm "$RUN_LOG" && touch "$RUN_LOG"
 
 #Only want to see errors
 git fetch >/dev/null 2>&1
 git pull --rebase origin master >/dev/null 2>&1
 if [ $? -ne 0 ] ; then
 	echo "Error pulling from master" >&2
-	git fetch >> /tmp/run_XCAB_cron.log 2>&1
-	git status >> /tmp/run_XCAB_cron.log 2>&1
-	git pull --rebase origin master >> /tmp/run_XCAB_cron.log 2>&1
-	echo "Error pulling from master" >> /tmp/run_XCAB_cron.log
-	cat /tmp/run_XCAB_cron_start.log /tmp/run_XCAB_cron.log | mail -s "run_XCAB_cron error and log" pdagent@me.com 
+	git fetch >> "$RUN_LOG" 2>&1
+	git status >> "$RUN_LOG" 2>&1
+	git pull --rebase origin master >> "$RUN_LOG" 2>&1
+	echo "Error pulling from master" >> "$RUN_LOG"
+	cat "$START_LOG" "$RUN_LOG" | mail -s "$my_dir error and log"  "$SUCCESS_EMAIL"
+	rm -f "$LOCKFILE"
 	exit 4
 fi
 git fetch  >/dev/null 2>&1
 
-$bin/sync_from_Dropbox.sh >> /tmp/run_XCAB_cron.log 2>&1
+$my_dir/sync_from_Dropbox.sh >> "$RUN_LOG" 2>&1
 
-$bin/build_and_notify.sh >> /tmp/run_XCAB_cron.log 2>&1
+$my_dir/build_and_notify.sh >> "$RUN_LOG" 2>&1
 
 #Only send mail if there's something to report
-if [  -s /tmp/run_XCAB_cron.log ] ; then
-	cat /tmp/run_XCAB_cron_start.log /tmp/run_XCAB_cron.log | mail -s "run_XCAB_cron log" pdagent@me.com 
+if [  -s "$RUN_LOG" ] ; then
+	cat "$START_LOG" "$RUN_LOG" | mail -s "$my_dir log" "$SUCCESS_EMAIL" 
+	rm -f "$LOCKFILE"
 fi
+
+rm -f "$LOCKFILE"
 
